e541838acbde515b7edd25457d69bbce3624d7c3
Jason M. Schwefel <Jason.Schwefel78@gmail.com>
2021-02-07 15:22:29 -0600

    Initial Commit

---
+++ Basic/StopNGo_NightMode/StopNGo_NightMode.ino
@@ -0,0 +1,181 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+#define aStop 19
+#define aYield 15
+#define aGo 14
+
+#define bStop 5
+#define bYield 4
+#define bGo 3
+
+#define nightSwitch 2
+
+bool nightMode = 0;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+//  aStop = 19
+//  aYield = 19
+//  aGo = 19
+
+  attachInterrupt(digitalPinToInterrupt(nightSwitch), nightSwitchISR, RISING);
+  
+  Serial.begin(9600);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(aStop, OUTPUT);
+  pinMode(aYield, OUTPUT);
+  pinMode(aGo, OUTPUT);
+
+  pinMode(bStop, OUTPUT);
+  pinMode(bYield, OUTPUT);
+  pinMode(bGo, OUTPUT);
+
+
+
+  digitalWrite(aStop, HIGH);
+  digitalWrite(bGo, HIGH);
+  delay(100);
+  digitalWrite(aYield, HIGH);
+  digitalWrite(bYield, HIGH);
+  delay(100);
+  digitalWrite(aGo, HIGH);
+  digitalWrite(bStop, HIGH);
+  delay(100);
+  
+  digitalWrite(aStop, LOW);
+  digitalWrite(bGo, LOW);
+  delay(100);
+  digitalWrite(aYield, LOW);
+  digitalWrite(bYield, LOW);
+  delay(100);
+  digitalWrite(aGo, LOW);
+  digitalWrite(bStop, LOW);
+  delay(100);
+
+  aStopSet();
+  bStopSet();
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //digitalWrite(aStop, HIGH);
+  //Serial.print(nightMode);
+  Serial.print(millis());
+  Serial.print("\n");
+
+if (nightMode) {
+  digitalWrite(aYield, HIGH);
+  digitalWrite(bStop, LOW);
+  delay(500);
+
+  digitalWrite(aYield, LOW);
+  digitalWrite(bStop, HIGH);
+  delay(500);
+}
+else {
+
+  aStopSet();
+  delay(500);
+  bGoSet();
+  delay(5000);
+
+  bYieldSet();
+  delay(1000);
+
+  bStopSet();
+  delay(500);
+  aGoSet();
+  delay(7500);
+
+  aYieldSet();
+  delay(1000);
+}
+}
+
+void aStopSet() {
+  digitalWrite(aStop, HIGH);
+  digitalWrite(aYield, LOW);
+  digitalWrite(aGo, LOW);
+}
+
+void aYieldSet() {
+  digitalWrite(aYield, HIGH);
+  digitalWrite(aGo, LOW);
+  digitalWrite(aStop, LOW);
+}
+
+void aGoSet() {
+  digitalWrite(aGo, HIGH);
+  digitalWrite(aStop, LOW);
+  digitalWrite(aYield, LOW);
+}
+
+
+void bStopSet() {
+  digitalWrite(bStop, HIGH);
+  digitalWrite(bYield, LOW);
+  digitalWrite(bGo, LOW);
+}
+
+void bYieldSet() {
+  digitalWrite(bYield, HIGH);
+  digitalWrite(bGo, LOW);
+  digitalWrite(bStop, LOW);
+}
+
+void bGoSet() {
+  digitalWrite(bGo, HIGH);
+  digitalWrite(bStop, LOW);
+  digitalWrite(bYield, LOW);
+}
+
+void nightSwitchISR() {
+  noInterrupts();
+  //Serial.print("\n");
+  //Serial.print(nightMode);
+  //Serial.print("\n");
+  //Serial.print("Night Mode ISR Pressed\n");
+  Serial.print(millis());
+  Serial.print("\t\tISR\n");
+  //delay(3000);
+  nightMode = !nightMode;
+  interrupts();
+}
+
+void PrintState(char *Seq) {
+  Serial.print(Seq);
+  Serial.print("\t-\t");
+  Serial.print(digitalRead(LED_BUILTIN));
+  Serial.print("\t");
+  Serial.print(digitalRead(aStop));
+  Serial.print("\t");
+  Serial.print(digitalRead(aYield));
+  Serial.print("\t");
+  Serial.print(digitalRead(aGo));
+  Serial.print("\n");
+
+}
